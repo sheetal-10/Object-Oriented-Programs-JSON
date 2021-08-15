386fdaf7ba429e8b0484536d3c9a5567522a2a61
sheetal-10 <sheetalp257@gmail.com>
2021-08-15 20:22:47 +0530

    Added: code for inventory details

--- src/com/opps/InventoryControll.java
+++ src/com/opps/InventoryControll.java
@@ -1,8 +1,22 @@
 package com.opps;
 
+import java.io.FileReader;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
 public class InventoryControll {
 	public static void main(String[] args) {
-		System.out.println("Welcome to OOPs problem including JSON");
+
+		JSONParser parser = new JSONParser();
+		try {
+				Object obj = parser.parse(new FileReader(".//Data/inventoryDetails.json"));
+				JSONObject jsonObject = (JSONObject) obj;
+				JSONArray inventory = (JSONArray) jsonObject.get("inventory");
 	
+				System.out.println(inventory);
+	
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 }
